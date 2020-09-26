2016874b9476d9ef62ee55d8558a634f655f3f4d
elhane <elhane2015@yandex.ru>
2020-09-26 16:40:19 +0500

    правит шапку проекта (+js для меню)

---
+++ source/js/menu.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function () {
+  var logo = document.querySelector('.header__logo-svg');
+  var header = document.querySelector('.header');
+  var headerToggle = document.querySelector('.header__toggle');
+
+  header.classList.remove('header--nojs');
+  logo.classList.remove('header__logo-svg--menu-opened');
+
+  headerToggle.addEventListener('click', function () {
+    if (header.classList.contains('header--closed')) {
+      header.classList.remove('header--closed');
+      header.classList.add('header--opened');
+      logo.classList.add('header__logo-svg--opened');
+    } else {
+      header.classList.add('header--closed');
+      header.classList.remove('header--opened');
+      logo.classList.remove('header__logo-svg--opened');
+    }
+  });
+})();
